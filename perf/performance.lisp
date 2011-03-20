a1310c5c1bf2e2f1131495e093aab6fcfa630ca8
Keith James <dev@deoxybyte.co.uk>
2011-03-20 01:13:20 +0000

    Copyright statement update

--- perf/performance.lisp
+++ perf/performance.lisp
@@ -1,7 +1,5 @@
 ;;;
-;;; Copyright (c) 2010, Keith James.
-;;;
-;;; All rights reserved.
+;;; Copyright (c) 2010-2011 Keith James. All rights reserved.
 ;;;
 ;;; Redistribution and use in source and binary forms, with or without
 ;;; modification, are permitted provided that the following conditions
